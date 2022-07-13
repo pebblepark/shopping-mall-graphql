c76cc6892a7e989b1936dbe55d93ea861266c48d
pebblepark <subling02@gmail.com>
2022-07-13 16:00:03 +0900

    Feat: React Query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { QueryClientProvider } from 'react-query';
 import { useRoutes } from 'react-router-dom';
+import { getClient } from './queryClient';
 import { routes } from './routes';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App() {
   const elem = useRoutes(routes);
-  return elem;
+  const queryClient = getClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {elem}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
