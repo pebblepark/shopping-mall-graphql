93af39d98ad9e8f18c288eaa92eace41de75f21b
pebblepark <subling02@gmail.com>
2022-07-14 16:02:31 +0900

    Chore: sass 라이브러리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './scss/index.scss';
 import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
