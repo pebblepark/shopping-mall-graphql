b665f8a8c7ed9a0dc4f9622b8e0ea3fb5037847c
pebblepark <subling02@gmail.com>
2022-07-14 16:01:59 +0900

    Feat: fetcher 함수 정의

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,5 +1,7 @@
 import { QueryClient } from 'react-query';
 
+type AnyOBJ = { [key: string]: any };
+
 export const getClient = (() => {
   let client: QueryClient | null = null;
   return () => {
@@ -7,3 +9,37 @@ export const getClient = (() => {
     return client;
   };
 })();
+
+const BASE_URL = 'https://fakestoreapi.com';
+
+export const fetcher = async ({
+  method,
+  path,
+  body,
+  params,
+}: {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+  path: string;
+  body?: AnyOBJ;
+  params?: AnyOBJ;
+}) => {
+  try {
+    const url = `${BASE_URL}${path}`;
+    const fetchOptions: RequestInit = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Contol-Allow-Origin': BASE_URL,
+      },
+    };
+    const res = await fetch(url, fetchOptions);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const QueryKeys = {
+  PRODUCTS: 'PRODUCTS',
+};
