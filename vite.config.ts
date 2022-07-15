6d438697fbca0ef3eb77ddc5e19fc91e52e24ba7
pebblepark <subling02@gmail.com>
2022-07-15 13:53:07 +0900

    Fix: 윈도우에서 제대로 동작하지 않는 vite-plugin-next-react-router 라이브러리 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { reactRouterPlugin } from 'vite-plugin-next-react-router';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouterPlugin()],
+  plugins: [react()],
   esbuild: {
     define: {
       this: 'window',
