0eea2484e75363d30dfbbd327abc6baa9bfc57bf
pebblepark <subling02@gmail.com>
2022-07-15 13:54:56 +0900

    Feat: Global Navigationbar 컴포넌트 추가

---
+++ src/components/gnb.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const Gnb = () => {
+  return (
+    <nav className='gnb'>
+      <ul>
+        {' '}
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to='/products'>상품목록</Link>
+        </li>
+        <li>
+          <Link to='/cart'>장바구니</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Gnb;
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Gnb from '../components/gnb';
 
 const Layout: React.FC = () => {
   return (
     <div>
+      <Gnb />
       <Suspense fallback={'loading...'}>
         <Outlet />
       </Suspense>
