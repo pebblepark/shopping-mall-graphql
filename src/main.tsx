d2f66a197393225a15cfb83a7ae6f1ae6af6cde8
pebblepark <subling02@gmail.com>
2022-07-22 01:11:30 +0900

    Feat: MSW setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './scss/index.scss';
 import App from './app';
+import { worker } from './mocks/browser';
+
+if (import.meta.env.DEV) {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
