e2a70c37ae2463dfa8ab6aa9e08c7b9c5a8e9644
pebblepark <subling02@gmail.com>
2022-08-04 22:55:15 +0900

    Fix: UPDATE_CART 호출 뒤 데이터 업데이트 방식 변경 (invalidateQueries -> optimistic updates)

--- src/components/cart/item.tsx
+++ src/components/cart/item.tsx
@@ -8,17 +8,40 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
 
   const { mutate: updateCart } = useMutation(
     ({ id, amount }: { id: string; amount: number }) =>
-      graphqlFetcher(UPDATE_CART, { id, amount })
+      graphqlFetcher(UPDATE_CART, { id, amount }),
+    {
+      onMutate: async ({ id, amount }) => {
+        await queryClient.cancelQueries(QueryKeys.CART);
+        const prevCart = queryClient.getQueryData<{
+          [key: string]: CartType[];
+        }>(QueryKeys.CART);
+
+        if (!prevCart?.[id]) return prevCart;
+
+        const newCart = {
+          ...prevCart,
+          [id]: { ...prevCart[id], amount },
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+
+        return prevCart;
+      },
+      onSuccess: (newValue) => {
+        const prevCart = queryClient.getQueryData<{
+          [key: string]: CartType[];
+        }>(QueryKeys.CART);
+        const newCart = {
+          ...prevCart,
+          [id]: newValue,
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+      },
+    }
   );
 
   const handleUpdateAmount = (e: SyntheticEvent) => {
     const value = Number((e.target as HTMLInputElement).value);
-    updateCart(
-      { id, amount: value },
-      {
-        onSuccess: () => queryClient.invalidateQueries(QueryKeys.CART),
-      }
-    );
+    updateCart({ id, amount: value });
   };
 
   return (
