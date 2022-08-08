3f5c65590b7f46a26a61fe8035c9a494287dd6f6
pebblepark <subling02@gmail.com>
2022-08-08 11:01:35 +0900

    Design: 결제창 modal css 추가

--- src/components/payment/modal.tsx
+++ src/components/payment/modal.tsx
@@ -16,11 +16,13 @@ const PaymentModal = ({
 }) => {
   return show ? (
     <ModalPortal>
-      <div>
-        <p>정말 결제하시겠습니까?</p>
-        <div>
-          <button onClick={proceed}>예</button>
-          <button onClick={cancel}>아니요</button>
+      <div className={`modal ${show ? 'show' : ''}`}>
+        <div className='modal__inner'>
+          <p>정말 결제하시겠습니까?</p>
+          <div>
+            <button onClick={proceed}>예</button>
+            <button onClick={cancel}>아니요</button>
+          </div>
         </div>
       </div>
     </ModalPortal>
