d6f137cc2bedf9448fd45929214afcb7b942625c
pebblepark <subling02@gmail.com>
2022-08-08 15:24:08 +0900

    Feat: apollo server resolvers 정의

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,25 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import schema from './schema';
+import resolvers from './resolvers';
+(async () => {
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+  });
+  /* context: {
+  } */
 
-const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers: undefined,
-  context: {},
-});
-
-const app = express();
-await server.start();
-server.applyMiddleware({
-  app,
-  path: '/graphql',
-  cors: {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-  },
-});
-await app.listen({ port: 8000 });
-console.log('server listening on 8000...');
+  const app = express();
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: {
+      origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+      credentials: true,
+    },
+  });
+  await app.listen({ port: 8000 });
+  console.log('server listening on 8000...');
+})();
---
+++ server/src/resolvers/types.ts
@@ -0,0 +1,10 @@
+export type Resolver = {
+  [k: string]: {
+    [key: string]: (
+      parent: any,
+      args: { [key: string]: any },
+      context: {},
+      info: any
+    ) => any;
+  };
+};
