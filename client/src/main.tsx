bed3f643ae0f4afc9708b3bbf8240f92147d610b
pebblepark <subling02@gmail.com>
2022-08-10 23:12:44 +0900

    Remove: 클라이언트 msw 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './scss/index.scss';
 import App from './app';
-import { worker } from './mocks/browser';
 import { RecoilRoot } from 'recoil';
-
-if (import.meta.env.DEV) {
-  worker.start();
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
