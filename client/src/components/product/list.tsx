247bd79ca347ded22ced3b85ae7aed2ccffdda51
pebblepark <subling02@gmail.com>
2022-08-11 18:02:26 +0900

    Feat: 상품목록 페이지 무한 스크롤 구현
    IntersectionObserver, React Query의 useInfiniteQuery 사용

--- client/src/components/product/list.tsx
+++ client/src/components/product/list.tsx
@@ -1,11 +1,13 @@
 import { Product } from '../../graphql/products';
 import ProductItem from './Item';
 
-const ProductList = ({ list }: { list: Product[] }) => (
+const ProductList = ({ list }: { list: { products: Product[] }[] }) => (
   <ul className='products'>
-    {list.map((product) => (
-      <ProductItem {...product} key={product.id} />
-    ))}
+    {list.map((page) =>
+      page.products.map((product) => (
+        <ProductItem {...product} key={product.id} />
+      ))
+    )}
   </ul>
 );
 
--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,17 +1,63 @@
-import { useQuery } from 'react-query';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import ProductList from '../../components/product/list';
 import GET_PRODUCTS, { Products } from '../../graphql/products';
 import { graphqlFetcher, QueryKeys } from '../../queryClient';
 
 const ProductListPage = () => {
-  const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
-    graphqlFetcher(GET_PRODUCTS)
-  );
+  const observerRef = useRef<IntersectionObserver>();
+  const fetchMoreRef = useRef<HTMLDivElement>(null);
+  const [intersecting, setIntersecting] = useState(false);
+
+  const getObserver = useCallback(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver((entries) => {
+        console.log(entries);
+        setIntersecting(entries[0]?.isIntersecting);
+      });
+
+      return observerRef.current;
+    }
+  }, [observerRef.current]);
+
+  useEffect(() => {
+    if (fetchMoreRef.current) getObserver()?.observe(fetchMoreRef.current);
+  }, [fetchMoreRef.current]);
+
+  const { data, isFetchingNextPage, fetchNextPage, hasNextPage, isSuccess } =
+    useInfiniteQuery<Products>(
+      QueryKeys.PRODUCTS,
+      ({ pageParam = '' }) =>
+        graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.products.at(-1)?.id;
+        },
+      }
+    );
+
+  /*
+  data: {
+    pages: [
+      { products: [...] },
+      { products: [...] },
+      { products: [...] },
+    ],
+    pageParams: [undefined, ...]
+  }
+  */
+
+  useEffect(() => {
+    if (!intersecting || !isSuccess || !hasNextPage || isFetchingNextPage)
+      return;
+    fetchNextPage();
+  }, [intersecting]);
 
   return (
     <div>
       <h2>상품목록</h2>
-      <ProductList list={data?.products || []} />
+      <ProductList list={data?.pages || []} />
+      <div ref={fetchMoreRef} />
     </div>
   );
 };
