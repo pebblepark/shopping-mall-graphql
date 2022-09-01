f75dd9d036fc39729f28821938f981161a4c728c
pebblepark <subling02@gmail.com>
2022-09-01 23:45:10 +0900

    Feat : 어드민페이지용 client graphql 추가 (add, update, delete product)

--- client/src/graphql/products.ts
+++ client/src/graphql/products.ts
@@ -39,4 +39,65 @@ export const GET_PRODUCT = gql`
   }
 `;
 
+export const ADD_PROUDCT = gql`
+  mutation ADD_PROUDCT(
+    $imageUrl: String!
+    $price: Int!
+    $title: String!
+    $description: String!
+  ) {
+    addProduct(
+      imageUrl: $imageUrl
+      price: $price
+      title: $title
+      description: $description
+    ) {
+      id
+      imageUrl
+      price
+      title
+      description
+      createdAt
+    }
+  }
+`;
+
+export const UPDATE_PRODUCT = gql`
+  mutation UPDATE_PRODUCT(
+    $id: ID!
+    $imageUrl: String
+    $price: Int
+    $title: String
+    $description: String
+  ) {
+    updateProduct(
+      id: $id
+      imageUrl: $imageUrl
+      price: $price
+      title: $title
+      description: $description
+    ) {
+      id
+      imageUrl
+      price
+      title
+      description
+      createdAt
+    }
+  }
+`;
+
+export const DELETE_PRODUCT = gql`
+  mutation DELETE_PRODUCT($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      imageUrl
+      price
+      title
+      description
+      createdAt
+    }
+  }
+`;
+
 export default GET_PRODUCTS;
